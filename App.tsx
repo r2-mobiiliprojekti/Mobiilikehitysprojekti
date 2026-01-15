7a37d1659669db6afed02095d3254d3847d57926
Jamo1993 <jamo.jaakola@gmail.com>
2026-01-15 17:48:58 +0200

    HomeScreen and navigation to screens

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from './Screens/HomeScreen'
+import FinSwe from './Screens/FinSwe'
+import SweFin from './Screens/SweFin'
+import ConnectWords from './Screens/ConnectWords'
+import PickWord from './Screens/PickWord'
+import { RootStackParamList } from './types/navigation'
+
+const Stack = createNativeStackNavigator<RootStackParamList>()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="FinSwe" component={FinSwe} />
+        <Stack.Screen name="SweFin" component={SweFin} />
+        <Stack.Screen name="ConnectWords" component={ConnectWords} />
+        <Stack.Screen name="PickWord" component={PickWord} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  
---
+++ Screens/SweFin.tsx
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../types/navigation'
+
+type Props = NativeStackScreenProps<RootStackParamList, 'SweFin'>
+
+export default function SweFin(_: Props) {
+  return (
+    <View style={styles.container}>
+      <Text>Haetaan sana ruotsiksi .xml tiedostosta, näytetään sen. Käyttäjä kirjoittaa sanan input kenttään suomeksi. vertaillaan tuloksia ja kerrotaan onko se oikein</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+})
