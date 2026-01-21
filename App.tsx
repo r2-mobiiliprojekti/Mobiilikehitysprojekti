b17d6cc7c5a68a6a6bbb5da20de08ed9d3b26608
jarmoit <jmarjakangas@gmail.com>
2026-01-21 16:49:34 +0200

    v2

--- App.tsx
+++ App.tsx
@@ -110,7 +110,6 @@ function MainAppNavigator() {
   );
 }
 
-const Stack = createNativeStackNavigator()
 export default function App() {
   const [currentUser, setCurrentUser] = useState<AppUser | null>(null);
   const [isLoading, setIsLoading] = useState(true);
