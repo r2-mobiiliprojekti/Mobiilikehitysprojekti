67d2706cb98fdf757e3cc3892a58d48d41d4e04c
jarmoit <jmarjakangas@gmail.com>
2026-01-21 12:53:42 +0200

    Fix case sensitivity issue for HomeScreen.tsx

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,9 @@
 import { View, Text, StyleSheet, Pressable } from 'react-native'
 import React from 'react'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { MainAppStackParamList } from '../types/navigation'
+import { RootStackParamList } from '../types/navigation'
 
-type Props = NativeStackScreenProps<MainAppStackParamList, 'Home'>
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>
 
 export default function HomeScreen({ navigation }: Props) {
   return (
@@ -24,12 +24,6 @@ export default function HomeScreen({ navigation }: Props) {
         <Text style={styles.buttonText}>Valitse oikea sana neljästä</Text>
       </Pressable>
 
-      <Pressable 
-        onPress={() => navigation.navigate('Profile')} 
-        style={[styles.button, styles.profileButton]}
-      >
-        <Text style={styles.buttonText}>Profiili</Text>
-      </Pressable>
     </View>
   )
 }
@@ -41,25 +35,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#ffe600',
     gap: 16,
-    padding: 20,
   },
   button: {
-    width: '100%',
-    maxWidth: 300,
+    width: 240,
     height: 80,
     backgroundColor: '#1612ee',
     borderRadius: 10,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  profileButton: {
-    backgroundColor: '#4CAF50',
-    marginTop: 30,
-  },
   buttonText: {
     color: '#fff',
     fontSize: 18,
     fontWeight: '600',
-    textAlign: 'center',
   },
 })
