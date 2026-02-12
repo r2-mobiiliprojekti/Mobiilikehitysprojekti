94dda24f4e28fe9c7b0e096f7c87ceb4b6077063
jarmoit <jmarjakangas@gmail.com>
2026-02-12 11:23:48 +0200

    Beep wrong answer ver1

--- Screens/ConnectWords.tsx
+++ Screens/ConnectWords.tsx
@@ -6,66 +6,62 @@ import Animated, {
   useSharedValue,
   useAnimatedStyle,
   withSpring
-} from "react-native-reanimated";
-import { GestureHandlerRootView, GestureDetector, Gesture } from "react-native-gesture-handler";
+} from "react-native-reanimated"
+import { GestureHandlerRootView, GestureDetector, Gesture } from "react-native-gesture-handler"
 import { getRandomSwedishWord } from '../Services/sanastoService'
 import type { Sanasto } from '../Types/sanasto'
-
-import { scheduleOnRN } from "react-native-worklets";
-import { Item, Layout, DragProps } from '../Types/connectwordtypes';
-import { usePlayAudioDing } from '../Services/audioService';
+import { scheduleOnRN } from "react-native-worklets"
+import { Item, Layout, DragProps } from '../Types/connectwordtypes'
+import { usePlayAudioDing, usePlayAudioWrongBeep } from '../Services/audioService'
 
 
 type Props = NativeStackScreenProps<MainAppStackParamList, 'ConnectWords'>
-
-const { width } = Dimensions.get("window");
-
+const { width } = Dimensions.get("window")
 
 
 export default function ConnectWords(_: Props) {
-  const [entry1, setEntry1] = useState<Sanasto>(getRandomSwedishWord());
-  const [entry2, setEntry2] = useState<Sanasto>(getRandomSwedishWord());
-  const [entry3, setEntry3] = useState<Sanasto>(getRandomSwedishWord());
-  const [targets, setTargets] = useState<Record<number, Layout>>({});
-  const [matched, setMatched] = useState<number[]>([]);
-  const playDing = usePlayAudioDing();
+  const [entry1, setEntry1] = useState<Sanasto>(getRandomSwedishWord())
+  const [entry2, setEntry2] = useState<Sanasto>(getRandomSwedishWord())
+  const [entry3, setEntry3] = useState<Sanasto>(getRandomSwedishWord())
+  const [targets, setTargets] = useState<Record<number, Layout>>({})
+  const [matched, setMatched] = useState<number[]>([])
+  const playDing = usePlayAudioDing()
   
 
   const handleMatch = useCallback((id: number) => {
-    setMatched((prev) => [...prev, id]);
-  }, []);
+    setMatched((prev) => [...prev, id])
+  }, [])
 
   function uudetSanat() {
-    setEntry1(getRandomSwedishWord());
-    setEntry2(getRandomSwedishWord());
-    setEntry3(getRandomSwedishWord());
-  };
+    setEntry1(getRandomSwedishWord())
+    setEntry2(getRandomSwedishWord())
+    setEntry3(getRandomSwedishWord())
+  }
 
   useEffect(() => {
     if (matched.length === 3) {
-      setMatched([]);
-      uudetSanat();
+      setMatched([])
+      uudetSanat()
     }})
   
-
   const ITEMS: Item[] = [
     { id: 1, SWE_word: entry1.swedish, FIN_word: entry1.translations},
     { id: 2, SWE_word: entry2.swedish, FIN_word: entry2.translations},
     { id: 3, SWE_word: entry3.swedish, FIN_word: entry3.translations},
-  ];
-  const shuffleArray = (array) => {
-    const newArray = [...array];
+  ]
+    const shuffleArray = (array: Item[]): Item[] => {
+    const newArray = [...array]
   
     for (let i = newArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+      const j = Math.floor(Math.random() * (i + 1))
+      const temp = newArray[i]
+      newArray[i] = newArray[j]
+      newArray[j] = temp
     }
   
-    return newArray;
-  };
-  
-  const shuffledITEMS = shuffleArray(ITEMS);
-
+    return newArray
+  }
+  const shuffledITEMS = shuffleArray(ITEMS)
 
   return (
     <>
@@ -77,7 +73,7 @@ export default function ConnectWords(_: Props) {
           <View
             key={item.id}
             onLayout={(e) => {
-              const layout = e.nativeEvent.layout;
+              const layout = e.nativeEvent.layout
 
               setTargets((prev) => ({
                 ...prev,
@@ -85,7 +81,7 @@ export default function ConnectWords(_: Props) {
                   x: layout.x + 60, // center adjust
                   y: layout.y + 180, // oli y: layout.y + 50 ,tää varmaan sopii näin
                 },
-              }));
+              }))
             }}
             style={styles.SWEWordBox}
           >
@@ -99,11 +95,12 @@ export default function ConnectWords(_: Props) {
       <View style={styles.FINWordBackground}>
         {shuffledITEMS.map((item) => {
           if (matched.includes(item.id)) {
-            playDing();
-            return null;
+            playDing()
+            return null
           }
+          
 
-          const target = targets[item.id];
+          const target = targets[item.id]
 
           return (
             <DraggableWord
@@ -112,73 +109,89 @@ export default function ConnectWords(_: Props) {
               target={target}
               onMatch={handleMatch}
             />
-          );
+          )
         })}
       </View>
-      
     </View>
-    
     </>
-  );
+  )
 }
 
-
-
-
 function DraggableWord({ item, target, onMatch }: DragProps) {
-  console.log("Rendering DraggableWord for item:", item);
-  const translateX = useSharedValue(0);
-  const translateY = useSharedValue(0);
-  const startX = useSharedValue(0);
-  const startY = useSharedValue(0);
+  console.log("Rendering DraggableWord for item:", item)
+  const translateX = useSharedValue(0)
+  const translateY = useSharedValue(0)
+  const startX = useSharedValue(0)
+  const startY = useSharedValue(0)
+  const playWrongBeep = usePlayAudioWrongBeep()
+  
+  
 
   const gesture = Gesture.Pan()
     .onStart(() => {
-      startX.value = translateX.value;
-      startY.value = translateY.value;
+      startX.value = translateX.value
+      startY.value = translateY.value
     })
     .onUpdate((e) => {
-      translateX.value = startX.value + e.translationX;
-      translateY.value = startY.value + e.translationY;
+      translateX.value = startX.value + e.translationX
+      translateY.value = startY.value + e.translationY
+      
     })
     .onEnd((e) => {
       if (!target) {
-        translateX.value = withSpring(0);
-        translateY.value = withSpring(0);
-        return;
+        translateX.value = withSpring(0)
+        translateY.value = withSpring(0)
+        
+        return
       }
 
       // Distance check
-      const dx = e.absoluteX - target.x;
-      const dy = e.absoluteY - target.y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
+      const dx = e.absoluteX - target.x
+      const dy = e.absoluteY - target.y
+      const distance = Math.sqrt(dx * dx + dy * dy)
 
-      const SNAP_DISTANCE = 50;
+      const SNAP_DISTANCE = 55
+      
+      
+      // measure distance and play wrong beep if close but wrong, all in this page is hardcoded :()
+      if (distance > SNAP_DISTANCE && e.absoluteY < 300) {
+        try {
+          if (playWrongBeep) {
+            scheduleOnRN(playWrongBeep);
+          } else {
+            console.error('playWrongBeep is not ready');
+          }
+        } catch (error) {
+          console.error('Error playing wrong beep:', error);
+        }
+      
+      }
 
       if (distance < SNAP_DISTANCE) {
         // snap animation
         translateX.value = withSpring(
           translateX.value + (target.x - e.absoluteX)
-        );
+        )
 
         translateY.value = withSpring(
           translateY.value + (target.y - e.absoluteY)
-        );
-
-        scheduleOnRN(onMatch, item.id);
+        )
+        console.log("y and x position on success:", target.y, target.x)
+        
+        scheduleOnRN(onMatch, item.id)
       } else {
         // return to origin
-        translateX.value = withSpring(0);
-        translateY.value = withSpring(0);
+        translateX.value = withSpring(0)
+        translateY.value = withSpring(0)
       }
-    });
+    })
 
   const style = useAnimatedStyle(() => ({
     transform: [
       { translateX: translateX.value },
       { translateY: translateY.value },
     ],
-  }));
+  }))
 
 
   return (
@@ -190,8 +203,9 @@ function DraggableWord({ item, target, onMatch }: DragProps) {
       </Animated.View>
     </GestureDetector>
     </GestureHandlerRootView>
-  );
+  )
 }
+
 
 
 const styles = StyleSheet.create({
@@ -212,11 +226,11 @@ const styles = StyleSheet.create({
   SWEWordBox: {
     width: 100,
     height: 100,
-    borderRadius: 16,
+    borderRadius: 6,
     backgroundColor: "#9e0",
     justifyContent: "center",
     alignItems: "center",
-    elevation: 2,
+    elevation: 1,
   },
   FINWordBackground: {
     backgroundColor: "green",
@@ -227,30 +241,23 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-
   FINWordBox: {
     flex: 0,
     maxHeight: "60%",
-
     backgroundColor: "#4CAF50",
-    
     paddingVertical: 14,
     borderRadius: 14,
     elevation: 5,
-    
     flexDirection: "column",
     flexWrap: "wrap",
     justifyContent: "center",
     alignContent: "stretch",
-
-
     paddingHorizontal: 12,
     gap: 16,
   },
-
   FINWordText: {
     color: "white",
     fontSize: 18,
     fontWeight: "600",
   },
-});
+})
